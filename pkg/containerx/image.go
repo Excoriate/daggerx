fd2d4f2acedc4723b311b7e47c018c5fcfd9a3fd
Alex Torres R <atorres.ruiz@hotmail.com>
2024-05-26 05:41:12 +0200

    feat: Add default image name logic (#26)
    Add logic to set a default image name if empty for images used in tests and functions in the codebase. Update tests to cover the new logic and ensure correct behavior.

--- pkg/containerx/image.go
+++ pkg/containerx/image.go
@@ -2,15 +2,54 @@ package containerx
 
 import (
 	"fmt"
+
 	"github.com/Excoriate/daggerx/pkg/fixtures"
 )
 
-// setDefaultIfEmpty returns a default version if the provided version is empty.
-func setDefaultIfEmpty(version string) string {
-	if version == "" {
-		return fixtures.ImageVersion
+// SetDefaultImageVersionIfEmpty returns a default version if the provided version is empty.
+// If both the provided version and the fallback version are empty, it returns the default version "latest".
+//
+// Parameters:
+//   - version: The primary version string.
+//   - fallbackVersion: The fallback version string.
+//
+// Returns:
+//   - The primary version if it is not empty.
+//   - The fallback version if the primary version is empty and the fallback version is not empty.
+//   - "latest" if both the primary and fallback versions are empty.
+func SetDefaultImageVersionIfEmpty(version, fallbackVersion string) string {
+	if version != "" {
+		return version
 	}
-	return version
+
+	if fallbackVersion != "" {
+		return fallbackVersion
+	}
+
+	return fixtures.ImageVersion
+}
+
+// SetDefaultImageNameIfEmpty returns a fallback image if the provided image is empty.
+// If both the provided image and the fallback image are empty, it returns the default image "alpine".
+//
+// Parameters:
+//   - image: The primary image string.
+//   - fallbackImage: The fallback image string.
+//
+// Returns:
+//   - The primary image if it is not empty.
+//   - The fallback image if the primary image is empty and the fallback image is not empty.
+//   - "alpine" if both the primary and fallback images are empty.
+func SetDefaultImageNameIfEmpty(image, fallbackImage string) string {
+	if image != "" {
+		return image
+	}
+
+	if fallbackImage != "" {
+		return fallbackImage
+	}
+
+	return fixtures.Image
 }
 
 type NewBaseContainerOpts struct {
@@ -20,23 +59,27 @@ type NewBaseContainerOpts struct {
 	Version string
 	// FallbackImage is the name of the fallback image to use if the primary image is empty.
 	FallbackImage string
+	// FallBackVersion is the version of the fallback image to use if the primary image is empty.
+	FallBackVersion string
 }
 
 // GetImageURL constructs the full image URL from the provided options.
-// It returns an error if the options are nil or the image name is empty.
+// It returns an error if the options are nil or if both the image and fallback image names are empty.
 //
 // Parameters:
-//   - opts: A pointer to NewBaseContainerOpts containing the image name and version.
+//   - opts: A pointer to NewBaseContainerOpts containing the image name, version, fallback image, and fallback version.
 //
 // Returns:
 //   - A string containing the full image URL in the format "image:version".
-//   - An error if the options are nil or the image name is empty.
+//   - An error if the options are nil or if both the image and fallback image names are empty.
 //
 // Example:
 //
 //	opts := &NewBaseContainerOpts{
-//	    Image:   "golang",
-//	    Version: "1.16",
+//	    Image:         "golang",
+//	    Version:       "1.16",
+//	    FallbackImage: "fallback-golang",
+//	    FallBackVersion: "1.15",
 //	}
 //	imageURL, err := GetImageURL(opts)
 //	if err != nil {
@@ -52,11 +95,8 @@ func GetImageURL(opts *NewBaseContainerOpts) (string, error) {
 		return "", fmt.Errorf("failed to create base container: both image and fallback image are empty")
 	}
 
-	if opts.Image != "" {
-		opts.Version = setDefaultIfEmpty(opts.Version)
-		return fmt.Sprintf("%s:%s", opts.Image, opts.Version), nil
-	}
+	opts.Image = SetDefaultImageNameIfEmpty(opts.Image, opts.FallbackImage)
+	opts.Version = SetDefaultImageVersionIfEmpty(opts.Version, opts.FallBackVersion)
 
-	// Use the fallback image if the primary image is empty.
-	return fmt.Sprintf("%s:%s", opts.FallbackImage, setDefaultIfEmpty(opts.Version)), nil
+	return fmt.Sprintf("%s:%s", opts.Image, opts.Version), nil
 }
