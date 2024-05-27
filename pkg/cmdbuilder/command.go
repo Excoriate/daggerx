87a2b4daad428275e10824c6df1609f156d50f2e
Alex Torres R <atorres.ruiz@hotmail.com>
2024-05-27 11:24:10 +0200

    feat: Add tests and implementation for converting DaggerCMD to string and generating sh command (#30)
    Added tests and implementation for converting DaggerCMD to a string and generating a sh command
    wrapped for execution. The ConvertCMDToString function converts a DaggerCMD slice to a string,
    preserving the command's consistency. The GenerateShCommand function generates the complete
    command wrapped for `sh -c` execution, handling arguments with spaces correctly.

--- pkg/cmdbuilder/command.go
+++ pkg/cmdbuilder/command.go
@@ -57,3 +57,63 @@ func GenerateCommand(command string, args ...string) (*types.DaggerCMD, error) {
 
 	return &cmdWithArgs, nil
 }
+
+// ConvertCMDToString converts a DaggerCMD slice to a string.
+// It preserves the consistency of the original command, handling arguments with spaces appropriately.
+//
+// Parameters:
+//   - cmd: A pointer to a DaggerCMD slice containing the command and its arguments.
+//
+// Returns:
+//   - A string representation of the command, suitable for direct execution.
+//
+// Example:
+//
+//	cmd := types.DaggerCMD{"go", "run", "main.go", "--verbose"}
+//	cmdString := ConvertCMDToString(&cmd)
+//	fmt.Println(cmdString) // Output: go run main.go --verbose
+//
+//	cmd = types.DaggerCMD{"terraform", "plan", "-var", "foo=bar", "apply --auto-approve"}
+//	cmdString = ConvertCMDToString(&cmd)
+//	fmt.Println(cmdString) // Output: terraform plan -var foo=bar apply --auto-approve
+func ConvertCMDToString(cmd *types.DaggerCMD) string {
+	return strings.Join(*cmd, " ")
+}
+
+// GenerateShCommand generates a command wrapped for execution using `sh -c`.
+// It ensures that arguments with spaces are correctly handled.
+//
+// Parameters:
+//   - command: A string representing the main command to be executed.
+//   - args: A variadic slice of strings representing the arguments for the command.
+//
+// Returns:
+//   - A string containing the complete command wrapped for `sh -c` execution.
+//   - An error if the main command is empty.
+//
+// Example:
+//
+//	cmd, err := GenerateShCommand("echo", "Hello, World!")
+//	if err != nil {
+//	    // handle error
+//	}
+//	fmt.Println(cmd) // Output: sh -c "echo Hello, World!"
+func GenerateShCommand(command string, args ...string) (string, error) {
+	// Validate the command
+	if command == "" {
+		return "", fmt.Errorf("command cannot be empty")
+	}
+
+	// Initialize the command slice with the main command
+	cmdWithArgs := types.DaggerCMD{command}
+
+	for _, arg := range args {
+		if arg == "" {
+			continue
+		}
+		cmdWithArgs = append(cmdWithArgs, arg)
+	}
+
+	cmdString := ConvertCMDToString(&cmdWithArgs)
+	return fmt.Sprintf("sh -c \"%s\"", cmdString), nil
+}
